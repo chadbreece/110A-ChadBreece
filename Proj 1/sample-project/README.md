d931ba4af11a12783ce77ed1202cf135a625c53f
Chad Breece <41607416+chadbreece@users.noreply.github.com>
2019-01-21 13:12:57 -0800

    0.8

---
+++ Proj 1/sample-project/README.md
@@ -0,0 +1,36 @@
+# Project 1: Steepest Descent Method
+
+Description: This sample project is aimed to show how to make a project submission. There are a few requirements about your project, this is beneficial for your later study in data science and programming. 
+
+## Compile/Run the code
+
+An example: In cmd(Windows) or console(Linux, Mac), type "python main.py" (" is not included) will run the code and the output is (file/in console). For MATLAB users, you can write: In MATLAB consolle, run some_project_code.m
+
+## Contributors
+This project acknowledges the following members:
+
+- Anoymous Who
+- Anoymous Anyone
+- Anoymous Nobody
+
+## Before making a submission 
+Your submission should include following necessary file. 
+
+- README.md 
+
+The ``README.md`` file is telling user a few important things about the project. For example:
+
+  0. Description of your project. (MUST INCLUDE)
+  1. How to run/compile your code? (MUST INCLUDE)
+  2. What kind of issues are known and need to be fixed? (OPTIONAL)
+  3. Contributors. (MUST INCLUDE if there are multiple contributors)
+  4. Todo list. (OPTIONAL)
+
+Sometimes we also include a file called ``requirements.txt``.
+
+The  ``requirements.txt`` usually tells user what packages are required to run the code, in optimization, we often use ``numpy``, ``scipy``, ``matplotlib``. 
+
+## Submission of your code
+When your code is ready, make sure your code is runnable. Python users can use the platform https://repl.it/languages/python3 to test your result. Once everything is good to go, you can tar/zip ALL files and submit it. 
+
+
---
+++ Proj 1/starter_code_kit.py
@@ -0,0 +1,58 @@
+# This starter code kit is written in Python and solves a sample problem with 
+# the exact steepest descent method (gradient descent method). 
+# 
+# 1. The objective function is f(x) = x[0]**2 + x[1]**4
+# 2. The optimal solution is (0, 0) obviously.
+
+import numpy as np
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize_scalar
+
+"""
+@def   : objective function 
+@param : x is vector 
+@return: a scalar
+"""
+def objective_func(x):
+    return x[0]**2 + x[1]**4
+
+"""
+@def   : gradient of objective function 
+@param : x is vector 
+@return: a vector 
+This is a test
+"""
+def grad_objective_func(x):
+    return np.array([2*x[0], 4*x[1]**3])
+
+# The exact steepest descent method. 
+# The step length is chosen to solve a one-dimensional optimization problem
+# The stopping criteria is selected to be 1e-9 for the gradient norm.
+# The starting point is [5, 5] for this problem
+# The optimal solution is [0., 0.].
+
+x0 = np.array([5, 5])
+x_opt = np.array([0., 0.])
+
+"""
+@def   : exact steepest descent method
+@param : x is vector 
+@return: a vector 
+"""
+def exact_steepest_descent(x0):             # input is the starting point
+    x = x0                                  # select the starting point
+    p = -grad_objective_func(x)             # find descent direction
+    while norm(p) > 1e-9:                   # if the norm is not small
+        def subproblem1d(alpha):            # define a 1d subproblem 
+            return objective_func(x + alpha * p)  
+                                            # use minimize_scalar function
+        res = minimize_scalar(subproblem1d) # to solve the subproblem
+        x = x + res.x * p                   # locate the next iterate
+        p = -grad_objective_func(x)         # find next descent direction
+
+    return x
+
+
+x = exact_steepest_descent(x0)
+print(x)
